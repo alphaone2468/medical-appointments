23f67ef304872d9e74cdbc5ce0aca388885a1ecd
alphaone2468 <129504534+alphaone2468@users.noreply.github.com>
2025-07-02 03:19:47 +0530

    Added Info

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const app=express();
 const cors=require("cors");
+require('dotenv').config();
 const mongoose=require("mongoose");
 const ActionTable = require("./models/action.model");
 
